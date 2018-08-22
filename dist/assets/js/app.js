d52b2d487ed13f9a32223c0b2510a1d5a3db3438
Qrac <qrac.jp@gmail.com>
2018-08-22 15:30:49 +0900

    Visible dist

---
+++ dist/assets/js/app.js
@@ -0,0 +1,18 @@
+/*! qranoko-starter-static v1.0.0 MIT by Qrac */
+
+"use strict";
+
+//----------------------------------------------------
+// Smooth scroll
+//----------------------------------------------------
+
+$(function () {
+  $('a[href^="#"]').on("click", function () {
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
